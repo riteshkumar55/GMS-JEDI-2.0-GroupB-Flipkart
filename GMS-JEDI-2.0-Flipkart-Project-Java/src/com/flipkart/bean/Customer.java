5bf6c5464cc4ef84dc8fee883d52949567941329
Deepak Prakash <deepakdpyqaz@gmail.com>
2023-06-21 09:24:33 +0530

    update artificats and file structure

--- GMS-JEDI-2.0-Flipkart-Project-Java/src/com/flipkart/bean/Customer.java
+++ GMS-JEDI-2.0-Flipkart-Project-Java/src/com/flipkart/bean/Customer.java
@@ -6,20 +6,51 @@ package com.flipkart.bean;
 /**
  * 
  */
-public class Customer extends User{
-	private String cusName;
-	private String address;
-	public String getCusName() {
-		return cusName;
+public class Customer extends User {
+	private String customerName;
+	private String customerAddress;
+	private String customerEmail;
+	private String customerDOB;
+	private String customerMobile;
+
+	public String getCustomerName() {
+		return customerName;
 	}
-	public void setCusName(String cusName) {
-		this.cusName = cusName;
+
+	public void setCustomerName(String name) {
+		this.customerName = name;
 	}
-	public String getAddress() {
-		return address;
+
+	public String getCustomerAddress() {
+		return customerAddress;
 	}
-	public void setAddress(String address) {
-		this.address = address;
+
+	public void setCustomerAddress(String address) {
+		this.customerAddress = address;
 	}
-	
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+	public String getCustomerDOB() {
+		return customerDOB;
+	}
+
+	public void setCustomerDOB(String customerDOB) {
+		this.customerDOB = customerDOB;
+	}
+
+	public String getCustomerMobile() {
+		return customerMobile;
+	}
+
+	public void setCustomerMobile(String customerMobile) {
+		this.customerMobile = customerMobile;
+	}
+
 }
